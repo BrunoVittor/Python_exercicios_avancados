d6a971e136215afe605ebcfa2e28255331c69437
bvittor <bvittor@f9c.com.br>
2022-02-01 21:16:40 -0300

    teste pycharm

--- definindo_funçoes.py
+++ definindo_funçoes.py
@@ -6,4 +6,6 @@ Funções são pquenos trechos de código que realizam tarefas específicas
 — pode ou não receber entradas de dados e rotornar uma saída de dados
 — muito úteis para executar procedimentos similares por repetidas vezes
 
+
+++++++++++++++++++++++++++++++++
 """
